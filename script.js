39cf543741ea52d5273acbcdb8167259530ba32c
ghost <138735754+Ryan20014737472@users.noreply.github.com>
2026-07-09 20:00:03 -0300

    Add event listeners for button and search input

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+// Mensagem de boas-vindas
+console.log("Receitas Chefe Dan 👨‍🍳 carregado com sucesso!");
 
+
+// Botão "Ver Receitas"
+
+const botao = document.querySelector(".botao");
+
+if (botao) {
+
+    botao.addEventListener("click", () => {
+
+        console.log("Abrindo página de receitas...");
+
+    });
+
+}
+
+
+// Barra de pesquisa
+
+const pesquisa = document.querySelector(".pesquisa input");
+
+if (pesquisa) {
+
+    pesquisa.addEventListener("input", () => {
+
+        console.log("Pesquisando:", pesquisa.value);
+
+    });
+
+}
